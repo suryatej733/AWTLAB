fe3ab281d922b1f8e83162a7822b348a05614282
suryatej733 <suryaregati52@gmail.com>
2022-03-09 08:44:43 +0530

    inherit 09/03/22

---
+++ inherit.js
@@ -0,0 +1,28 @@
+class student 
+{
+    constructor(name)
+    {
+        this.name=name;
+    }
+    display()
+    {
+        console.log("student name in parent class:"+this.name);
+    }
+}
+
+class student1 extends student
+{
+    constructor(name,roll)
+    {
+        super(name);
+        this.roll=roll;
+    }
+    display1()
+    {
+        console.log("student roll:"+this.roll);
+    }
+}
+
+let s=new student1("hello",1251);
+s.display();
+s.display1();
